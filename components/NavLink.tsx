99f16f4729f5e85d461128d4f40d42145e522368
luxu <luxu3883606@gmail.com>
2025-12-30 10:14:48 +0800

    fix: refactor NavLink to use next/link and remove react-router-dom dependency

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -1,26 +1,31 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
+"use client";
+
+import Link, { LinkProps } from "next/link";
+import { usePathname } from "next/navigation";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
-  className?: string;
+interface NavLinkPropsCompat extends Omit<LinkProps, "href" | "className"> {
+  to: string;
+  className?: string; // Allow string className
   activeClassName?: string;
-  pendingClassName?: string;
+  children?: React.ReactNode;
 }
 
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+const NavLink = forwardRef<HTMLAnchorElement, NavLinkPropsCompat>(
+  ({ to, className, activeClassName, ...props }, ref) => {
+    const pathname = usePathname();
+    const isActive = pathname === to;
+
     return (
-      <RouterNavLink
+      <Link
         ref={ref}
-        to={to}
-        className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
-        }
+        href={to}
+        className={cn(className, isActive && activeClassName)}
         {...props}
       />
     );
-  },
+  }
 );
 
 NavLink.displayName = "NavLink";
