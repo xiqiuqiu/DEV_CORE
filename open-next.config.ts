1e9fe0e4c172611d5432f9ce6f5bac291f479f41
luxu <luxu3883606@gmail.com>
2025-12-30 10:11:35 +0800

    chore: add open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,8 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+    /*
+     * You can customize the Cloudflare adapter configuration here.
+     * For typical deployments, the defaults are sufficient.
+     */
+});
